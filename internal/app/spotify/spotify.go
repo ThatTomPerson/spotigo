946671ce86a9240c4e10f193fd7a073c40c41ded
Thomas Albrighton <thomasalbrighton@gmail.com>
2018-03-29 09:22:04 +1030

    starting to make the first packet

--- internal/app/spotify/spotify.go
+++ internal/app/spotify/spotify.go
@@ -4,15 +4,24 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"net"
 	"net/http"
 
 	"github.com/davecgh/go-spew/spew"
 
 	_ "gitlab.com/ThatTomPerson/spotify-go/internal/pkg/api/authentication"
+	"gitlab.com/ThatTomPerson/spotify-go/internal/pkg/api/keyexchange"
 )
 
 type ApList struct {
 	ApList []string `json:"ap_list"`
+}
+
+func (l *ApList) Rand() string {
+	t := len(l.ApList)
+	i := rand.Intn(t)
+	return l.ApList[i]
 }
 
 type Spotify struct {
@@ -28,8 +37,27 @@ func (s *Spotify) Connect(ctx context.Context) error {
 		return fmt.Errorf("couldn't find Spotify APs: %v", err)
 	}
 
+	ap := aps.Rand()
+
+	c, err := net.Dial("tcp", ap)
+	if err != nil {
+		return fmt.Errorf("couldn't connect to Spotify AP %s: %v", ap, err)
+	}
+
+	var version uint64 = 1
+
+	hello := keyexchange.ClientHello{
+		BuildInfo: &keyexchange.BuildInfo{
+			Product:      keyexchange.Product_PRODUCT_LIBSPOTIFY.Enum(),
+			ProductFlags: []keyexchange.ProductFlags{keyexchange.ProductFlags_PRODUCT_FLAG_DEV_BUILD},
+			Platform:     keyexchange.Platform_PLATFORM_OSX_X86_64.Enum(),
+			Version:      &version,
+		},
+		FingerprintsSupported: []keyexchange.Fingerprint{Fingerprint_FINGERPRINT_GRAIN},
+	}
+
 	// net.Dial()
-	spew.Dump(aps)
+	spew.Dump(ap)
 	return nil
 }
 
